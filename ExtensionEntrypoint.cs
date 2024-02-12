6dd9b22c562583ac896700d1d89b4848fd6818a2
Vladislav Rudakov <vladislav.rudakoff@gmail.com>
2024-02-13 02:17:31 +0300

    init project

---
+++ ExtensionEntrypoint.cs
@@ -0,0 +1,30 @@
+﻿namespace FastSnippets;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Extensibility;
+
+/// <summary>
+/// Extension entrypoint for the VisualStudio.Extensibility extension.
+/// </summary>
+[VisualStudioContribution]
+internal class ExtensionEntrypoint : Extension
+{
+    /// <inheritdoc/>
+    public override ExtensionConfiguration ExtensionConfiguration => new()
+    {
+        Metadata = new(
+                id: "FastSnippets.a6f8d01c-776d-49ea-9dd0-3a03a2184f25",
+                version: this.ExtensionAssemblyVersion,
+                publisherName: "Publisher name",
+                displayName: "FastSnippets",
+                description: "Extension description"),
+    };
+
+    /// <inheritdoc />
+    protected override void InitializeServices(IServiceCollection serviceCollection)
+    {
+        base.InitializeServices(serviceCollection);
+
+        // You can configure dependency injection here by adding services to the serviceCollection.
+    }
+}
